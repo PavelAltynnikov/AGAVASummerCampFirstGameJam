94d79de80b74001ac784be4200d834cc23c486b3
PavelAltynnikov <robenzon11@gmail.com>
2023-08-05 19:08:18 +0400

    add DrunkPlayer class

---
+++ Assets/Scripts/DrunkPlayer.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+
+public class DrunkPlayer : Player
+{
+    [SerializeField] private float _speed;
+    [SerializeField] private Rigidbody _rb;
+
+    private Vector3 _direction;
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        if (_speed <= 0)
+        {
+            Debug.LogError("Ошибка перемещения. Скорость игрока должна быть больше 0");
+        }
+
+        float horizontalInput = 0;
+        float verticalInput = 0;
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            verticalInput = 1;
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            verticalInput = -1;
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            horizontalInput = -1;
+        }
+        else if (Input.GetKey(KeyCode.Z))
+        {
+            horizontalInput = 1;
+        }
+
+        _direction.x = horizontalInput * _speed;
+        _direction.z = verticalInput * _speed;
+
+        _rb.velocity = _direction;
+    }
+}
